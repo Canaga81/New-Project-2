21564d4feba969bf2c57713fad31e61fb80b5a90
Canaga81 <Canagamemmedov@gmail.com>
2023-08-31 16:26:20 +0400

    Swiper End

---
+++ swiper.js
@@ -0,0 +1,8 @@
+let swiper = new Swiper(".mySwiper", {
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    slidesPerView: 4,
+    spaceBetween: 20,
+  });
