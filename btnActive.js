7dd21c5a9346364f8b6c2ebb51cd0e4ad9feb68e
Canaga81 <Canagamemmedov@gmail.com>
2023-09-05 16:23:05 +0400

    Finish Modal

---
+++ btnActive.js
@@ -0,0 +1,19 @@
+const btnActive = document.getElementById('prdc-btn');
+const activeContainer = document.querySelector('.active-container');
+const prdcContainer = document.querySelector('.prdc-container');
+const closeBtn = document.querySelector(".modal_close_btn")
+
+btnActive.addEventListener("click", () => {
+    activeContainer.classList.add('bg-gray');
+    prdcContainer.classList.add('active');
+});
+
+closeBtn.addEventListener("click",()=> {
+    activeContainer.classList.remove('bg-gray');
+    prdcContainer.classList.remove('active');
+}) 
+
+// btnActive.addEventListener("click", () => {
+//     activeContainer.classList.remove('bg-gray');
+//     prdcContainer.classList.remove('active')
+// });
