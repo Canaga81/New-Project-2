61a4f0451ca6b321095245614235399030a17c2d
Canaga81 <Canagamemmedov@gmail.com>
2023-09-06 15:02:29 +0400

    Update

--- btnActive.js
+++ btnActive.js
@@ -1,7 +1,7 @@
 const btnActive = document.getElementById('prdc-btn');
 const activeContainer = document.querySelector('.active-container');
 const prdcContainer = document.querySelector('.prdc-container');
-const closeBtn = document.querySelector(".modal_close_btn")
+const closeBtn = document.querySelector(".modal_close_btn");
 
 btnActive.addEventListener("click", () => {
     activeContainer.classList.add('bg-gray');
@@ -13,7 +13,3 @@ closeBtn.addEventListener("click",()=> {
     prdcContainer.classList.remove('active');
 }) 
 
-// btnActive.addEventListener("click", () => {
-//     activeContainer.classList.remove('bg-gray');
-//     prdcContainer.classList.remove('active')
-// });
