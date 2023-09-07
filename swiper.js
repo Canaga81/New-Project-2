80d7cd9d6ac4c02ec554c79ee0171eb7e2a95db1
Canaga81 <Canagamemmedov@gmail.com>
2023-09-07 12:50:02 +0400

    Choose Section Start 840px Responsive

--- swiper.js
+++ swiper.js
@@ -6,7 +6,25 @@ let swiper = new Swiper(".mySwiper", {
   },
   slidesPerView: 4,
   spaceBetween: 0,
-});
+
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 2,
+      spaceBetween: 20
+    },
+    // when window width is >= 480px
+    480: {
+      slidesPerView: 3,
+      spaceBetween: 30
+    },
+    // when window width is >= 640px
+    840: {
+      slidesPerView: 3,
+      spaceBetween: 40
+    }
+
+}});
 
 
 let swiper2 = new Swiper(".youSwiper", {
@@ -16,4 +34,22 @@ let swiper2 = new Swiper(".youSwiper", {
   autoplay: {
     delay: 1500,
   },
-});
+
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 2,
+      spaceBetween: 20
+    },
+    // when window width is >= 480px
+    480: {
+      slidesPerView: 3,
+      spaceBetween: 30
+    },
+    // when window width is >= 640px
+    840: {
+      slidesPerView: 4,
+      spaceBetween: 20
+    }
+
+}});
