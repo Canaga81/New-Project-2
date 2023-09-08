2ea28b500bfe29819f185bd9d004d8e42765e8f5
Canaga81 <Canagamemmedov@gmail.com>
2023-09-08 20:43:52 +0400

    Btn-Icon Responsive

--- headerActive.js
+++ headerActive.js
@@ -2,7 +2,7 @@ const xMarkButton = document.querySelector('.icon-mark');
 const barsButton = document.querySelector(".icon-list");
 
 const picture = document.getElementById("header");
-const pictureActive = document.querySelector(".header-active")
+const pictureActive = document.querySelector(".header-active");
 
 barsButton.addEventListener("click", () => {
     pictureActive.style.display = "block";
@@ -12,4 +12,14 @@ barsButton.addEventListener("click", () => {
 xMarkButton.addEventListener("click", () => {
     pictureActive.style.display = "none";
     picture.style.display = "block";
-})
+});
+
+
+//! ----------------------------------------------------------------
+
+const faChevronDown = document.querySelector(".fa-chevron-down");
+const btnIcon = document.querySelector(".btn-icon");
+
+faChevronDown.addEventListener("click", () => {
+    btnIcon.classList.toggle('active');
+});
