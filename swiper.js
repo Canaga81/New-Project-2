a5cb4a1f879014ee182dba0d06c26b84c5ef2bab
Canaga81 <Canagamemmedov@gmail.com>
2023-09-08 13:34:40 +0400

    Responsive 99

--- swiper.js
+++ swiper.js
@@ -9,20 +9,24 @@ let swiper = new Swiper(".mySwiper", {
 
   breakpoints: {
     // when window width is >= 320px
-    320: {
+    0: {
       slidesPerView: 2,
       spaceBetween: 20
     },
     // when window width is >= 480px
     557: {
-      slidesPerView: 2,
+      slidesPerView: 3,
       spaceBetween: 10
     },
     // when window width is >= 640px
     840: {
-      slidesPerView: 3,
+      slidesPerView: 4,
       spaceBetween: 40
-    }
+    },
+    // 1440:{
+    //   slidesPerView: 4,
+    //   spaceBetween: 0
+    // }
 }});
 
 
@@ -36,18 +40,18 @@ let swiper2 = new Swiper(".youSwiper", {
 
   breakpoints: {
     // when window width is >= 320px
-    320: {
+    0: {
       slidesPerView: 2,
       spaceBetween: 20
     },
     // when window width is >= 480px
     480: {
-      slidesPerView: 3,
+      slidesPerView: 4,
       spaceBetween: 30
     },
     // when window width is >= 640px
     840: {
-      slidesPerView: 4,
+      slidesPerView: 6,
       spaceBetween: 20
     }
 
