353633737bcd9f321ca426c15f97f6e659cbee67
Canaga81 <Canagamemmedov@gmail.com>
2023-09-09 16:05:27 +0400

    Blog Responsive Finsish

--- swiper.js
+++ swiper.js
@@ -29,6 +29,37 @@ let swiper = new Swiper(".mySwiper", {
     },
 }});
 
+let swiper3 = new Swiper(".heSwiper", {
+  loop:true,
+  navigation: {
+    nextEl: ".fa-arrow-right",
+    prevEl: ".fa-arrow-left",
+  },
+  slidesPerView: 4,
+  spaceBetween: 0,
+
+  breakpoints: {
+    // when window width is >= 320px
+    0: {
+      slidesPerView: 2,
+      spaceBetween: 10
+    },
+    413: {
+      slidesPerView: 2,
+      spaceBetween: 0
+    },
+    // when window width is >= 480px
+    557: {
+      slidesPerView: 3,
+      spaceBetween: 10
+    },
+    // when window width is >= 640px
+    840: {
+      slidesPerView: 4,
+      spaceBetween: 40
+    },
+}});
+
 
 let swiper2 = new Swiper(".youSwiper", {
   loop:true,
